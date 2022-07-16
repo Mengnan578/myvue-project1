55e67266f1ad02387148bd69e34cf7cacf3b2a8a
孟南 <76862521+Mengnan578@users.noreply.github.com>
2022-07-16 19:42:16 +0800

    fix: 跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 // 在编写代码的时候关闭eslink开发工具
 module.exports = {
-    lintOnSave: false
+    // 管理eslink校验
+    lintOnSave: false,
+    // 跨域
+    devServer: {
+        proxy: {
+            //会把请求路径中的/api换为后面的代理服务器
+            '/api': {
+                //提供数据的服务器地址
+                target: 'http://gmall-h5-api.atguigu.cn',
+                pathrEWRITE: { "^/api": "" },
+            }
+        },
+    }
 }
